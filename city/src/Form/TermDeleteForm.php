da3b978764750e1a98dd75b88cfdd7839bba1a1f
Deepikabawa <31303657+Deepikabawa@users.noreply.github.com>
2024-07-12 00:58:35 +0530

    Add files via upload

---
+++ city/src/Form/TermDeleteForm.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\city\Form\TermDeleteForm.
+ */
+
+namespace Drupal\city\Form;
+
+use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provides a form for deleting a content_entity_example entity.
+ *
+ * @ingroup city
+ */
+class TermDeleteForm extends ContentEntityConfirmFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete entity %name?', array('%name' => $this->entity->label()));
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * If the delete command is canceled, return to the contact list.
+   */
+  public function getCancelUrl() {
+    return new Url('entity.city_term.collection');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Delete the entity and log the event. logger() replaces the watchdog.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity = $this->getEntity();
+    $entity->delete();
+
+    $this->logger('city')->notice('deleted %title.',
+      array(
+        '%title' => $this->entity->label(),
+      ));
+    // Redirect to term list after delete.
+    $form_state->setRedirect('entity.city_term.collection');
+  }
+
+}
